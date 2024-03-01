d9e57c7341e8a7f1cd7708e7261df61f3c675b8b
thianchai-c <thianchai.c@riseplus.tech>
2024-03-01 15:30:33 +0700

    stocks sorting

--- Helpers/QueryObject.cs
+++ Helpers/QueryObject.cs
@@ -9,5 +9,7 @@ namespace app_dotnet.Helpers
     {
         public string? Symbol { get; set; } = null;
         public string? CompanyName { get; set; } = null;
+        public string? SortBy { get; set; } = null;
+        public bool IsDecsending { get; set; } = false;
     }
 }
--- Repository/StockRepository.cs
+++ Repository/StockRepository.cs
@@ -46,9 +46,18 @@ namespace app_dotnet.Repository
             {
                 stocks = stocks.Where(s => s.CompanyName.Contains(query.CompanyName));
             }
+
             if (!string.IsNullOrWhiteSpace(query.Symbol))
             {
                 stocks = stocks.Where(s => s.Symbol.Contains(query.Symbol));
+            }
+
+            if (!string.IsNullOrWhiteSpace(query.SortBy))
+            {
+               if (query.SortBy.Equals("Symbol", StringComparison.OrdinalIgnoreCase))
+               {
+                 stocks = query.IsDecsending ?stocks.OrderByDescending(s => s.Symbol) :stocks.OrderBy(s => s.Symbol);
+               }
             }
             return await stocks.ToListAsync();
         }
